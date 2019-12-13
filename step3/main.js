aa1eea6a2b0f2637becef6139ac7441b1d6fcf8b
giuviglia94 <vigliaturo94@gmail.com>
2019-12-13 13:05:16 +0100

    step 3 grafico

--- step3/main.js
+++ step3/main.js
@@ -1,8 +1,10 @@
 function printFatturato(type,data) {
   var ctx = document.getElementById("fatturato").getContext("2d");
   new Chart(ctx, {
+
     type: type,
     data: {
+
       labels: moment.months(),
       datasets: [
         {
@@ -55,6 +57,56 @@ function printFatturatoAgent(type, label, data) {
   });
 }
 
+function printTeam(type,data1,data2,data3) {
+  var canvas = document.getElementById('team_efficiency').getContext("2d");
+  new Chart(canvas, {
+    type: type,
+    data: {
+      labels: ['1', '2', '3', '4', '5'],
+      datasets: [{
+        label: 'Team1',
+        yAxisID: 'A',
+        data: data1,
+        borderColor:"rgb(57, 92, 56)"
+      },
+      {
+        label: 'Team2',
+        yAxisID: 'B',
+        data: data2,
+        borderColor:"rgb(222, 53, 16)"
+      },
+      {
+        label: 'Team3',
+        yAxisID: 'C',
+        data: data3,
+        borderColor:"rgb(185, 144, 23)"
+      }
+    ]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          id: 'A',
+          type: 'linear',
+          position: 'left',
+        },
+        {
+          id: 'B',
+          type: 'linear',
+          position: 'left',
+        },
+
+          {
+            id: 'C',
+            type: 'linear',
+            position: 'left',
+        }
+      ]
+      }
+    }
+  });
+}
+
 
 
 
@@ -84,6 +136,7 @@ function getDataPie(){
       var label = Object.keys(data["fatturato_by_agent"]["data"]);
       //con object.keys mi prendo i nomi degli agenti
       var data = Object.values(data["fatturato_by_agent"]["data"]);
+
       //con object.values mi prendo i valori associati a ogni nome
       //estraggo direttamente dal file php i parametri che mi servono e li metto in delle variabili
       printFatturatoAgent(
@@ -99,8 +152,36 @@ function getDataPie(){
   })
 }
 
+function getDataTeam(){
+  $.ajax ({
+    url:"server.php",
+    method:'GET',
+    success: function(data){
+      console.log("team",data);
+      var type = data["team_efficiency"]["type"];
+      var label = Object.keys(data["team_efficiency"]["data"]);
+      console.log("arrayteam",label);
+      //con object.keys mi prendo i nomi degli agenti
+      var data1 = Object.values(data["team_efficiency"]["data"]['Team1']);
+      var data2 = Object.values(data["team_efficiency"]["data"]['Team2']);
+      var data3 = Object.values(data["team_efficiency"]["data"]['Team3']);
+      console.log("arraydat",data1);
+
+      //con object.values mi prendo i valori associati a ogni nome
+      //estraggo direttamente dal file php i parametri che mi servono e li metto in delle variabili
+      printTeam(type,data1,data2,data3);
+
+
+    },
+    error: function(err) {
+      console.log("error",err);
+    }
+  })
+}
+
 function init() {
   getDataLine();
   getDataPie();
+  getDataTeam();
 }
 $(document).ready(init);
---
+++ step3/server.php
@@ -0,0 +1,6 @@
+<?php
+
+header('Content-Type: application/json');
+include 'databasetre.php';
+echo json_encode($graphs);
+ ?>
